00d27b1b6a11db92a8636f9a46bfc0f9cc286d74
Ricardo Dantas Goncalves <ricardo@dyna.mo>
2021-02-24 21:43:47 +0100

    Add dragging message

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import icon from '../assets/icon.svg';
 import './App.global.css';
 
 import onSelectFilesClick from './actions/onSelectFilesClick';
 import onFilesDropped from './actions/onFilesDropped';
+import DropFilesHere from './components/DropFilesHere';
 
 const Hello = () => {
+  const [draggingStatus, setDraggingStatus] = useState('stopped');
+
   document.addEventListener('drop', (event) => {
+    setDraggingStatus('stopped');
     event.preventDefault();
     event.stopPropagation();
     onFilesDropped(event);
   });
 
   document.addEventListener('dragover', (e) => {
+    // setDraggingStatus('stopped');
     e.preventDefault();
     e.stopPropagation();
   });
 
-  // document.addEventListener('dragenter', (event) => {
-  //   console.log('File is in the Drop Space');
-  // });
+  document.addEventListener('dragenter', (event) => {
+    console.log('File is in the Drop Space');
+    setDraggingStatus('started');
+  });
 
-  // document.addEventListener('dragleave', (event) => {
-  //   console.log('File has left the Drop Space');
-  // });
+  document.addEventListener('dragleave', (event) => {
+    console.log('File has left the Drop Space');
+    setDraggingStatus('stopped');
+  });
+
+  if (draggingStatus === 'started') {
+    return <DropFilesHere />;
+  }
 
   return (
     <div>
---
+++ src/components/DropFilesHere.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './DropFilesHere.global.css';
+
+const DropFilesHere = () => {
+  return (
+    <div>
+      <div className="DropFilesHere">
+        <div className="DropFilesHere-Message">Drop your files</div>
+      </div>
+    </div>
+  );
+};
+
+export default DropFilesHere;
