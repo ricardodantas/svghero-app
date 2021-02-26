854d31f2bf15bfd4091af6ba0f9e8348925e3bc8
Ricardo Dantas Goncalves <ricardo@dyna.mo>
2021-02-26 23:14:07 +0100

    bum version to v 0.2.2

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const config = Object.freeze({
   appName: 'SvgHero',
   website: 'https://svghero.app',
-  version: '0.2.1',
+  version: '0.2.2',
   credits: 'Ricardo Dantas Gonçalves',
   authors: ['Ricardo Dantas Gonçalves'],
   routes: {
