1ab29841ea47f0f5409e40bc6203efc094f0cd02
Ricardo Dantas <ricardodantas@gmail.com>
2021-03-04 10:42:26 +0100

    fix text color on dropzone component

--- src/components/DropZone.tsx
+++ src/components/DropZone.tsx
@@ -24,13 +24,13 @@ const Message = styled.div({
 
 const DropFilesHere = () => {
   return (
-    <div className="no-scroll height-size-full align-center-xy flex-direction-column">
+    <div className="no-scroll height-size-full align-center-xy flex-direction-column title">
       <Icon
         icon={IconNames.DOWNLOAD}
         iconSize={Icon.SIZE_LARGE}
         className="animate__infinite animate__animated animate__fadeInDown"
       />
-      <Message className="title">{translate('drop_files_now')}</Message>
+      <Message>{translate('drop_files_now')}</Message>
     </div>
   );
 };
