1acbb9923b9507c6eb62813d15e499ddba541c9b
Ricardo Dantas <ricardodantas@gmail.com>
2021-03-04 12:30:42 +0100

    fix lint

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { Icon } from '@blueprintjs/core';
 import { Link, useHistory } from 'react-router-dom';
 import { IconNames } from '@blueprintjs/icons';
