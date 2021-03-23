dff6eaee77265667d8baaf1f03e9215a69b1b644
Ricardo Dantas <ricardodantas@gmail.com>
2021-03-23 21:46:16 +0100

    add Mac App Store packaging

---
+++ release-mas.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export DISTRIBUTION=mas
+yarn release:mas
--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { FocusStyleManager } from '@blueprintjs/core';
 
+import is from 'electron-is';
 import Home from './pages/Home';
 import Preferences from './pages/Preferences';
 import ActivateLicense from './pages/ActivateLicense';
@@ -11,7 +12,9 @@ import ProtectedRoute from './components/ProtectedRoute';
 import { setFirstUseDate } from './actions/renderer/license';
 
 export default function App() {
-  setFirstUseDate();
+  if (!is.mas()) {
+    setFirstUseDate();
+  }
   FocusStyleManager.onlyShowFocusOnTabs();
   return (
     <Router>
@@ -28,11 +31,13 @@ export default function App() {
           path={AppConfig.routes.preferences}
           component={Preferences}
         /> */}
-        <Route
-          exact
-          path={AppConfig.routes.activateLicense}
-          component={ActivateLicense}
-        />
+        {!is.mas() && (
+          <Route
+            exact
+            path={AppConfig.routes.activateLicense}
+            component={ActivateLicense}
+          />
+        )}
       </Switch>
     </Router>
   );
